1d9e64dc09cd3089f74602c6b804d2c717862f7a
Maxud-R <roslyakmaxym@gmail.com>
2023-05-17 19:03:13 -0500

    Fix Update->Start in UIScript, added link to script

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -7,14 +7,22 @@ public class UIScript : MonoBehaviour {
 	public Text hptext;
 	public Image hbar;
 	public GameObject player;
-    void Update() {
+    void Start() {
 		StartCoroutine(HealthUpdate());
     }
     IEnumerator HealthUpdate() {
+		bool playerDead = false;
+		PlayerControls script = player.GetComponent<PlayerControls>();
 		for (;;) {
-			if (player != null) { //use here C# link variables type
-				hptext.text = player.GetComponent<PlayerControls>().health + "/100";
-				hbar.fillAmount = player.GetComponent<PlayerControls>().health/100f;
+			if (player != null) {
+				if (playerDead) {
+					script = player.GetComponent<PlayerControls>();
+					playerDead = false;
+				}
+				hptext.text = script.health + "/100";
+				hbar.fillAmount = script.health/100f;
+			} else {
+				playerDead = true;
 			}
 			yield return new WaitForSeconds(.3f);
 		}

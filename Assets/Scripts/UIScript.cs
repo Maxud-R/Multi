404d091d8a3e52a2eae03eebdc1f95c65f7f478e
Maxud-R <roslyakmaxym@gmail.com>
2023-05-18 22:04:08 -0500

    Description text, nickname show

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+//using Photon.Realtime;
 
 public class UIScript : MonoBehaviour {
 	public Text hptext;
+	public Text desc;
 	public Image hbar;
 	public GameObject player;
+	private GameObject cam;
+	private PlayerControls script;
+	public RaycastHit look;
     void Start() {
-		StartCoroutine(HealthUpdate());
+		StartCoroutine(RareChecks());
     }
-    IEnumerator HealthUpdate() {
+    IEnumerator RareChecks() {
 		bool playerDead = false;
-		PlayerControls script = player.GetComponent<PlayerControls>();
+		script = player.GetComponent<PlayerControls>();
+		cam = GameObject.FindWithTag("MainCamera");
 		for (;;) {
+			//HealthUpdate
 			if (player != null) {
 				if (playerDead) {
 					script = player.GetComponent<PlayerControls>();
@@ -23,6 +31,16 @@ public class UIScript : MonoBehaviour {
 				hbar.fillAmount = script.health/100f;
 			} else {
 				playerDead = true;
+			}
+			//Description text
+			if (Physics.Raycast(cam.transform.position, cam.transform.forward, out look, 10f)) {
+				if (look.collider.gameObject.CompareTag("Player")) {
+					desc.text = look.collider.gameObject.GetComponent<PhotonView>().Controller.NickName;
+				} else {
+					desc.text = look.collider.gameObject.name;
+				}
+			} else {
+				desc.text = "";
 			}
 			yield return new WaitForSeconds(.3f);
 		}

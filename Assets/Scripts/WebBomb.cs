57a13357f0beb5b316cdc407ca234716ecbad151
Maxud-R <roslyakmaxym@gmail.com>
2023-07-01 23:56:58 -0500

    Working on webBomb generator

--- Assets/Scripts/WebBomb.cs
+++ Assets/Scripts/WebBomb.cs
@@ -5,38 +5,53 @@ using Photon.Pun;
 
 public class WebBomb : MonoBehaviour {
 
-	public GameObject fragment;
-	public Material errorMaterial;
-	private GameObject thatFragment;
-	private GameObject prevFragment;
 	private const float RADIUS = 0.3f;
-	private const float DISTANCE = 0.5f;
+	private const float DISTANCE = 0.6f;
 	private const int LENGTH = 7;
 	private const int RAY_COUNT = 5;
+	public GameObject fragment;
+	public Material errorMaterial;
+	private GameObject prevFragment;
+	private GameObject[,] list = new GameObject[RAY_COUNT, LENGTH];
+	private Vector3 throwDir;
 	
 	void Start() {
-		StartCoroutine(Delay());
+		throwDir = transform.position;
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) Destroy(gameObject);
 	}
 	
     IEnumerator Delay() {
-		yield return new WaitForSeconds(2f); //not needed to stop, it is automatically when destroy;
-		PhotonNetwork.Destroy(gameObject); //this bomb will be destroyed in 2 seconds
+		yield return new WaitForSeconds(2f); 
+		Generate();
     }
+    void OnDestroy() {
+		for (int i = 0; i < RAY_COUNT; i++) {
+			for (int b = 0; b < LENGTH; b++) {
+				Destroy(list[i, b]);
+			}
+		}
+	}
     
-	void OnDestroy() {
+	void Generate() { //TOFIX: rays are generated only when enough room for full length. First fragment too far from bomb.
 		int attempts = 5;
 		for (int i = 0; i < RAY_COUNT; i++) {
 			Vector3 pos = new Vector3(Random.Range(-RADIUS, RADIUS), Random.Range(-RADIUS, RADIUS), Random.Range(-RADIUS, RADIUS));
-			if (Physics.Raycast(transform.position, pos, (float)LENGTH*0.36f)) {
+			pos += throwDir.normalized/2f;
+			if (!Physics.Raycast(transform.position, pos, (float)LENGTH*0.36f)) {
 				Vector3 point = transform.position;
 				for (int b = 0; b < LENGTH; b++) {
-					bool createNext = false;
-					if (Physics.Raycast(point, pos, DISTANCE)) createNext = true;
-					thatFragment = Instantiate(fragment, point + pos.normalized/1.7f, Quaternion.LookRotation(pos));
-					if (createNext) thatFragment.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = errorMaterial;
-					if (b > 0) prevFragment.GetComponentInChildren<HingeJoint>().connectedBody = thatFragment.GetComponentInChildren<Rigidbody>();
-					point = thatFragment.transform.position;
-					prevFragment = thatFragment;
+					if (!Physics.Raycast(point, pos, DISTANCE)) {
+						list[i, b] = Instantiate(fragment, point + pos.normalized/1.7f, Quaternion.LookRotation(pos));
+						if (b > 0) prevFragment.GetComponentInChildren<HingeJoint>().connectedBody = list[i, b].GetComponentInChildren<Rigidbody>();
+						else list[i, b].GetComponentInChildren<Rigidbody>().isKinematic = true;
+						point = list[i, b].transform.position;
+						prevFragment = list[i, b];
+					} else {
+						if (prevFragment != null) prevFragment.GetComponentInChildren<Rigidbody>().isKinematic = true;
+					}
 				}
 			} else {
 				attempts--;
@@ -48,6 +63,11 @@ public class WebBomb : MonoBehaviour {
 	
 	void OnCollisionEnter(Collision data) {
 		if (data.gameObject.name == "PlayerInterface(Clone)") PhotonNetwork.Destroy(gameObject);
-		else gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		else {
+			gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			throwDir = Vector3.zero;
+			//throwDir -= transform.position;
+			Generate();
+		}
 	}
 }

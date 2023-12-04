4ffceb63507e198a95adb994ff6d1b53adcdba7e
Maxud-R <roslyakmaxym@gmail.com>
2023-12-03 23:35:51 -0600

    Deleted all from map and created new obstacles

--- Assets/Scripts/Destruction.cs
+++ Assets/Scripts/Destruction.cs
@@ -6,14 +6,15 @@ public class Destruction : MonoBehaviour {
     [SerializeField] private int destState = 0;
     private int destStatePrev;
     private const int MESH_PIECES = 5;
-    private static Vector3Int slices = new Vector3Int(2, 1, 7);
-    private Vector3[] splittedSize = new Vector3[slices.x*slices.y*slices.z];
+    [SerializeField]private Vector3[] splittedSize;
     public GameObject expl;
     public Mesh[] states = new Mesh[MESH_PIECES]; 
     public GameObject partSysPref;
     private ParticleSystem partSys;
     void Start() {
         destStatePrev = destState;
+        BoxCollider a = gameObject.GetComponent<BoxCollider>();
+        Vector3Int slices = new Vector3Int((int)(a.bounds.size.x/0.5f), (int)(a.bounds.size.y/0.5f), (int)(a.bounds.size.z/0.5f));
         splittedSize = SplitSize(gameObject.GetComponent<BoxCollider>().bounds.size, slices);
         partSys = GameObject.Find("StabPieces").GetComponent<ParticleSystem>();
     }
@@ -34,20 +35,23 @@ public class Destruction : MonoBehaviour {
                     partSys.transform.rotation = Quaternion.LookRotation(a - shockwave.transform.position, transform.up);
                     partSys.Emit(1);
                     pointsCount++;
-                    Debug.DrawRay(a, transform.TransformPoint(Vector3.up), Color.red, 1.5f);
                 }
             }
-            destState = (int)Mathf.Ceil((MESH_PIECES-1)*(Mathf.Round(pointsCount/((float)splittedSize.Length/100))/100));
+            Destroy(gameObject);
+            //destState = (int)Mathf.Ceil((MESH_PIECES-1)*(Mathf.Round(pointsCount/((float)splittedSize.Length/100))/100));
         }
     }
     Vector3[] SplitSize(Vector3 size, Vector3Int pieces) {
+        Debug.Log(pieces);
         Vector3 corner = transform.position - size/2;
         Vector3[] pieceCentres = new Vector3[pieces.x*pieces.y*pieces.z];
         for(int x = 0; x < pieces.x; x++) {
+            for(int y = 0; y < pieces.y; y++) {
                 for(int z = 0; z < pieces.z; z++) {
-                    Vector3 point = new Vector3(corner.x+(size.x/pieces.x)*x+(size.x/pieces.x)/2, transform.position.y ,corner.z+(size.z/pieces.z)*z+(size.z/pieces.z)/2);
-                    pieceCentres[(x*pieces.z)+z] = point;
+                    Vector3 point = new Vector3(corner.x+(size.x/pieces.x)*x+(size.x/pieces.x)/2, corner.y+(size.y/pieces.y)*y+(size.y/pieces.y)/2, corner.z+(size.z/pieces.z)*z+(size.z/pieces.z)/2);
+                    pieceCentres[z+y*pieces.z+x*pieces.z*pieces.y] = point;
                 }
+            }
         }
         return pieceCentres;
     }
